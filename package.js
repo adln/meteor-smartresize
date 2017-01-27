0df6c621166ebd716366a40caba590d83b9e605d
Gharbi Adlen <adlen025@yahoo.fr>
2017-01-27 23:41:19 +0000

    Publish to Atmosphere

--- package.js
+++ package.js
@@ -4,7 +4,7 @@ Package.describe({
   // Brief, one-line summary of the package.
   summary: 'JQuery Smartresize for Meteor',
   // URL to the Git repository containing the source code for this package.
-  git: '',
+  git: 'https://github.com/adln/meteor-smartresize/tree/master',
   // By default, Meteor will default to using README.md for documentation.
   // To avoid submitting documentation, set this field to null.
   documentation: 'README.md'
@@ -13,7 +13,8 @@ Package.describe({
 Package.onUse(function (api) {
   api.versionsFrom('1.4.2.3');
   api.use('ecmascript');
-  api.mainModule('smartresize.js');
+  api.use('jquery', 'client');
+  api.mainModule('lib/smartresize.js', 'client');
 });
 
 Package.onTest(function (api) {
